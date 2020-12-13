4625e3a9ffaf3ae7ebacd952f2d86d96b682f7c5
Marta Stekla <75528807+steklamarta@users.noreply.github.com>
2020-12-13 17:26:19 +0100

    Add files via upload

--- game.h
+++ game.h
@@ -11,21 +11,86 @@ class Game {
 };
 
 class Object: protected Game {
+protected:
+    string m_objectName;
+    string m_objectType;
+public:
 
 };
 
 class Weapon : protected Object {
+private:
+    int m_attackBonus;
+    bool m_equippedWeapon;
+public:
+    Weapon (string weaponName, int attackBonus){
+        m_objectName = weaponName;
+        m_attackBonus = attackBonus;
+        m_objectType = "weapon";
+        m_equippedWeapon = false;
+    };
 
+    int getAttackBonus (){
+        return m_attackBonus;
+    }
 };
 
 class Potion : protected Object {
-
+private:
+    int m_healHP;
+public:
+    Potion (string potionName, int healHP) {
+        m_objectName = potionName;
+        m_healHP = healHP;
+        m_objectType = "potion";
+    };
 };
 
 // konci Object zacina Character
 
 class Character : protected Game {
+protected:
+    string m_characterName;
+    int m_characterAttack;
+    int m_characterBaseAttack;
+    int m_characterHP;
+    int m_characterMaxHP;
+    bool m_characterType; // true = PC, false = NPC
+public:
+    int getAttack (){
+        return m_characterAttack;
+    };
 
+    string getCharacterName (){
+        return m_characterName;
+    };
+
+    int getCharacterHP (){
+        return m_characterHP;
+    };
+
+    int getChacterMaxHP (){
+        return m_characterMaxHP;
+    };
+
+    void printInfo (){
+        cout << "Name: " << m_characterName << endl;
+        cout << "HP: " << m_characterHP << endl;
+        cout << "Attack: " << m_characterAttack << endl; // vratit se sem a pridat bonus ze zbrane
+    }
+
+    Character (string characterName, int characterBaseAttack, int characterMaxHP, bool characterType){
+        m_characterName = characterName;
+        m_characterBaseAttack = characterBaseAttack;
+        m_characterAttack = m_characterBaseAttack;
+        m_characterMaxHP = characterMaxHP;
+        m_characterHP = m_characterMaxHP;
+        m_characterType = characterType;
+    };
+
+    void Attack (){
+        
+    }
 };
 
 class Player : protected Character {
